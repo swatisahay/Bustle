dbb5d5f82524a1107bfad5e7445a2ea26155b56b
Guest User <Guest@epicodus-94.local>
2018-08-17 15:41:17 -0700

    add body component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
+import { Imageslide1Component } from './imageslide1/imageslide1.component';
 
 
 @NgModule({
@@ -13,7 +15,9 @@ import { FooterComponent } from './footer/footer.component';
     AppComponent,
     NavbarComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    BodyComponent,
+    Imageslide1Component
   ],
   imports: [
     BrowserModule
