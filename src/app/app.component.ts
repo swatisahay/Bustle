9da048f760a81ce8037f58c9206a2fcb1043f95d
Guest User <Guest@epicodus-94.local>
2018-08-20 08:45:29 -0700

    create environment folder and updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import { apiKey } from './env';
 })
 export class AppComponent {
   title = 'app';
-  // private apiUrl = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=a6946e64856b44fdad020dbc4ecd6492'
+
   private apiUrl = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey='+apiKey()
   data: any = {};
   constructor(private http: Http){
