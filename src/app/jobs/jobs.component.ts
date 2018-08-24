b347c4a20789b55d7c24c3f1852183308a68ebb5
Swati Sahay <swatiranjan0111@gmail.com>
2018-08-24 15:16:12 -0700

    trying to add methods in jobs component

--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Jobs } from '../jobs.model';
+import { Router } from '@angular/router';
+import { BustleService } from '../bustle.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-jobs',
   templateUrl: './jobs.component.html',
-  styleUrls: ['./jobs.component.css']
+  styleUrls: ['./jobs.component.css'],
+  providers: [BustleService]
 })
 export class JobsComponent implements OnInit {
 
-  constructor() { }
+  careers: FirebaseListObservable<any[]>;
+     currentRoute: string = this.router.url;
 
-  ngOnInit() {
-  }
+     constructor(private router: Router, private bustleService: BustleService){}
+
+     ngOnInit(){
+       this.careers = this.bustleService.getJobs();
+     }
+
+     goToDetailJobs(clickedJob) {
+       this.router.navigate(['jobs', clickedJob.$key]);
+     };
 
 }
